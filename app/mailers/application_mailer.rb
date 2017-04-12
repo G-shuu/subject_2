f65be9b440e0770665a147e254ddcd222e4a3a68
大塚脩司 <otsukashuji@MacBookAir.local>
2017-04-13 03:05:41 +0900

    amend

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'from@example.com',
            bcc: 'eng.trinity.24.so.6263@gmail.com'
   layout 'mailer'
 end
